99e4fd47364aa1371736cec64cc0c7cea3aa5f6c
Venkatasalam123 <88706466+Venkatasalam123@users.noreply.github.com>
2024-04-04 11:52:40 +0530

    Add files via upload

---
+++ environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // apiUrl: 'http://dev.learndevopswithme.xyz/api',
+  // apiUrl: 'http://a99a9b4e26e454177a294e8e8249082e-2047849348.us-west-2.elb.amazonaws.com/api',
+  // apiUrl: 'http://13.114.94.62:5000/api',
+  // apiUrl: 'http://100.20.64.155:30000/api'
+  // apiUrl: 'http://test.learndevopswithme.xyz/api'
+  apiUrl: 'http://10.1.171.99/api',
+};
